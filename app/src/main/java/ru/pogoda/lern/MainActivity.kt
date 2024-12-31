c3f97cd766cade46f93f37b09af640561676792d
StarikovSS <Chahlikn1@gmail.com>
2024-12-31 14:18:10 +0500

    Новый проект.

---
+++ app/src/main/java/ru/pogoda/lern/MainActivity.kt
@@ -0,0 +1,15 @@
+package ru.pogoda.lern
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val r:Int = 0
+        val w:Int = 123
+
+     }
+}
